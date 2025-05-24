3a9f1b2ee0ca6a3a6132d141f5487d1b86bb9658
Samuel T. <samitale86@gmail.com>
2025-05-24 22:17:34 +0300

    some modification

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,9 @@
 import express from "express"
-import authRoute from "./routes/auth.route.js"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 
+import authRoute from "./routes/auth.route.js"
+import messageRoute from "./routes/message.route.js"
 import connectDB from "./lib/db.js"
 
 dotenv.config()
@@ -10,7 +12,12 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
 app.use("/api/auth", authRoute)
+app.use("/api/message", messageRoute)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
