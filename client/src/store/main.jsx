5d8d32b73213e1c556f1ae25608c4f2f173a8b12
sami855-ux <samitale56@outlook.com>
2025-06-15 16:21:49 +0300

    Add user reducer to Redux store configuration

--- client/src/store/main.jsx
+++ client/src/store/main.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit"
 
 import authReducer from "./auth.js"
+import userReducer from "./user.js"
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    user: userReducer,
   },
 })
 
