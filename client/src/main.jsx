a81a3eb9582e167c961fd048e34ee496df0f9654
sami855-ux <samitale56@outlook.com>
2025-10-15 20:26:03 +0300

    refactored the code like spacing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,12 @@
-import ReactDOM from "react-dom/client"
-import App from "./App.jsx"
-import "./index.css"
-import { Provider } from "react-redux"
-import store from "./store/main.jsx"
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+import { Provider } from "react-redux";
+import store from "./store/main.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
   </Provider>
-)
+);
