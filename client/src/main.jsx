8a71b3cae1fd3eb69ce9d3c887be880c1e2e10c6
sami855-ux <samitale56@outlook.com>
2025-10-17 15:36:23 +0300

    react query providers are added to the project

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,9 +4,17 @@ import "./index.css";
 
 import { Provider } from "react-redux";
 import store from "./store/main.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+
+      <App />
+    </QueryClientProvider>
   </Provider>
 );
