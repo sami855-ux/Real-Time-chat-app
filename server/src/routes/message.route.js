42049920525d514d1d0d8080220801f3bee4664e
sami855-ux <samitale56@outlook.com>
2025-10-22 16:12:55 +0300

    refactore and new route is added

--- server/src/routes/message.route.js
+++ server/src/routes/message.route.js
@@ -1,24 +1,29 @@
-import express from "express";
-import { protectRoute } from "../middleware/auth.middleware.js";
+import express from "express"
+import { protectRoute } from "../middleware/auth.middleware.js"
 import {
   getMessages,
   getUserConversations,
   sendMessage,
   getOrCreateConversation,
-} from "../controller/message.controller.js";
+  markConversationAsRead,
+} from "../controller/message.controller.js"
+import upload from "../middleware/multer.middleware.js"
 
-const router = express.Router();
+const router = express.Router()
 
 // Create or get a conversation between two users
-router.post("/:receiverId", getOrCreateConversation);
+router.post("/:receiverId", getOrCreateConversation)
 
 //send a message to user
-router.post("/sender/:id", sendMessage);
+router.post("/sender/:id", protectRoute, upload.array("images", 4), sendMessage)
 
 // Get a message  from conversation
-router.get("/conversation/:conversationId", getMessages);
+router.get("/conversation/:conversationId", getMessages)
 
 // Get all users from conversation
-router.get("/conversation", protectRoute, getUserConversations);
+router.get("/conversation", protectRoute, getUserConversations)
 
-export default router;
+//Set isRead messages to true
+router.put("/read/:conversationId", markConversationAsRead)
+
+export default router
